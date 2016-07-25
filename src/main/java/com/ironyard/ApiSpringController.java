3d5e295657d363edcf07d94950077d56e9c89490
jeffryporter <jporter7@gmail.com>
2016-07-25 10:50:57 -0400

    Add Background Thread

--- src/main/java/com/ironyard/ApiSpringController.java
+++ src/main/java/com/ironyard/ApiSpringController.java
@@ -5,16 +5,46 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.PostConstruct;
 import java.util.HashMap;
 
 /**
  * Created by jeffryporter on 7/25/16.
  */
+
+
+
 @RestController
 public class ApiSpringController
 {
     static final String API_URL = "http://gturnquist-quoters.cfapps.io/api/random";
 
+    @PostConstruct
+    public synchronized void init() throws InterruptedException
+    {
+        Thread t = new Thread()
+        {
+            @Override
+            public void run()
+            {
+                while(true)
+                {
+                    System.out.println("hi mom.");
+                    try
+                    {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        t.start();
+    }
+
+
+    //https://github.com/Book-It/BookIt
     @RequestMapping (path ="/quote", method = RequestMethod.GET)
     public HashMap getQuote()
     {
